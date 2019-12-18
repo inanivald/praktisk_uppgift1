f3297bdb2946cec676750ac9ae67aff3fa374f0f
Ina Nivald <inanivald@hotmail.com>
2019-12-18 19:20:29 +0100

    Added users input text to the deezer url. Then put the result of the search into the artist card.

--- app.js
+++ app.js
@@ -1,39 +1,35 @@
 
 const resultBox = document.querySelector("#result-box");
 const searchfieldEl = document.querySelector('#searchfield');
-const searchTextEl = document.querySelector('#textInputSearch'); 
+
+let croppedUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?q=";
+
+     const getSearch = async (url) => {
+
+        const response = await fetch(url, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+                "x-rapidapi-key": "398fde3a05msh308a8e39370118dp1b26bdjsneb357ac3324a"
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`Response was not OK. Status code: ${response.status}.`);
+        }
+        return await response.json();
+    }; 
+    
  
+//add user input to end of dezzer url
+
 // Catch value from input-field and save it
  // Use eventlistener when user clicks on search-button
 searchfieldEl.addEventListener('submit', function(e) {
- e.preventDefault();
- 
- let search = searchTextEl.value;
- 
- console.log(search);
- 
- searchTextEl.value = '';
-
-});
-
-
-const getSearch = async () => {
-    let newUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?q="
-
-    const response = await fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=sia", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-            "x-rapidapi-key": "398fde3a05msh308a8e39370118dp1b26bdjsneb357ac3324a"
-        }
-    });
-    if (!response.ok) {
-        throw new Error(`Response was not OK. Status code: ${response.status}.`);
-    }
-    return await response.json();
-}; 
-const renderSearchResult = () => {
-    getSearch().then(response => {
+    e.preventDefault();
+    let searchTextEl = document.querySelector('#textInputSearch').value;
+    croppedUrl += searchTextEl;
+    console.log(croppedUrl);
+    getSearch(croppedUrl).then(response => {
         // loop over result
         response.data.forEach(data => {
             const artist = `
@@ -49,8 +45,8 @@ const renderSearchResult = () => {
             resultBox.innerHTML = artist;
         });
     })
-};
-getSearch().then(renderSearchResult).catch(err => {
+
+.then().catch(err => {
     console.log("Error getting search result!");
 });
-
+});
